2fe58bc72ae6c95079af3ac5c49e7585b49146f6
Stefano <67341828+stefanoturcarelli@users.noreply.github.com>
2024-06-03 13:28:44 -0500

    Add CounterOne component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import CounterOne from "./components/CounterOne";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <CounterOne />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/CounterOne.jsx
@@ -0,0 +1,34 @@
+// The following code does not update the component because React renders
+// 'number' only once, when the page is loaded.
+
+function CounterOne() {
+  let number = 0;
+
+  const print = () => {
+    console.clear();
+    console.log(number);
+  };
+
+  const decrement = () => {
+    number--;
+    print();
+  };
+
+  const increment = () => {
+    number++;
+    print();
+  };
+
+  return (
+    <section>
+      <h2>Updating a variable without state (bad)</h2>
+      <div className="counter">
+        <input type="button" onClick={decrement} value="Minus" />
+        <input type="text" value={number} readOnly />
+        <input type="button" onClick={increment} value="Plus" />
+      </div>
+    </section>
+  );
+}
+
+export default CounterOne;
