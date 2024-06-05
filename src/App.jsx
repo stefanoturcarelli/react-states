857d7e40221f461b9606a5c3aa981496c4b5a707
Stefano <67341828+stefanoturcarelli@users.noreply.github.com>
2024-06-05 11:33:48 -0500

    chore: Remove unused code and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import CounterOne from "./components/CounterOne";
 import CounterTwo from "./components/CounterTwo";
@@ -9,8 +6,6 @@ import Toggle from "./components/Toggle";
 import CounterFour from "./components/CounterFour";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <CounterOne />
